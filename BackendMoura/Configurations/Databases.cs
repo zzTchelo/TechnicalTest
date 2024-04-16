fd02b46f6b040b460e21efe4a3631d19f72b95bd
Joao_Marcelo_PC <jmarcelo06@outlook.com>
2024-04-16 18:45:29 -0300

    Models | Configurations criados

---
+++ BackendMoura/Configurations/Databases.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace BackendMoura.Configurations
+{
+    public class Databases
+    {
+        public static String getStringConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+    }
+}
---
+++ BackendMoura/Models/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendMoura.Models
+{
+    public class Pessoa
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Inativo { get; set; }
+        public NacionalidadeEnum Nacionalidade { get; set; }
+        public string RG { get; set; }
+        public string Passaporte { get; set; }
+    }
+
+    public enum NacionalidadeEnum
+    {
+        BR = 1,
+        Outros = 2
+    }
+
+}
