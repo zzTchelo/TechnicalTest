1a76c9d48bd8522430f7dcdb3be2969653ef6fdd
Joao_Marcelo_PC <jmarcelo06@outlook.com>
2024-04-17 17:47:37 -0300

    CSS página de cadastro

--- FrontendMoura/src/app/Application/objects/headerMenu.ts
+++ FrontendMoura/src/app/Application/objects/headerMenu.ts
@@ -1,6 +1,6 @@
 export const Menu: { name: string, link: string }[] = [
   { name: 'Listar todos', link: '/ListarTodos' },
-  { name: 'Pesquisar', link: '/Pesquisar' },
+  //{ name: 'Pesquisar', link: '/Pesquisar' },
   { name: 'Cadastrar', link: '/Cadastrar' }
   //{ name: 'Alterar', link: '/Alterar'},
   //{ name: 'Deletar', link: '' }
