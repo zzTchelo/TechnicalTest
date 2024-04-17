e92826c374c39cd3309d870988f1b36364b7d5a0
Joao_Marcelo_PC <jmarcelo06@outlook.com>
2024-04-16 21:02:08 -0300

    Script do banco adicionado

---
+++ Database/Criacao_Tabela.sql
@@ -0,0 +1,19 @@
+use tecnicoMoura;
+
+CREATE TABLE Pessoa (
+    Codigo INT IDENTITY(1,1) PRIMARY KEY,
+    Nome VARCHAR(50) not null,
+    DataNascimento DATE,
+    Inativo BIT not null,
+    Nacionalidade SMALLINT CHECK (Nacionalidade IN (1, 2)) not null,
+    RG VARCHAR(20),
+    Passaporte VARCHAR(20)
+);
+
+select * from Pessoa;
+
+
+INSERT INTO Pessoa (Nome, DataNascimento, Inativo, Nacionalidade, RG, Passaporte)
+VALUES 
+('João', '1990-05-15', 0, 1, '123456', 'AB123456'),
+('Maria', '1985-08-20', 1, 2, '987654', 'CD987654');
